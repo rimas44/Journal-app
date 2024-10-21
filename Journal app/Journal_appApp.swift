6911e3d0afb6e02359b9bbdc0e500c045ccc5d11
rimas44 <ralshahrani24@twq.idserve.net>
2024-10-21 14:23:38 +0300

    Initial Commit

---
+++ Journal app/Journal_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_appApp.swift
+//  Journal app
+//
+//  Created by Rimas Alshahrani on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Journal_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Journal appTests/Journal_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_appTests.swift
+//  Journal appTests
+//
+//  Created by Rimas Alshahrani on 18/04/1446 AH.
+//
+
+import Testing
+@testable import Journal_app
+
+struct Journal_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
